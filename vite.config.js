d37e0bc7ffc0c64e1090926cbea3af517a8f2ffd
linjianyou <linjianyou@live.cn>
2025-01-24 19:29:23 +0800

    start_url: "/home-page/",

--- vite.config.js
+++ vite.config.js
@@ -48,7 +48,7 @@ export default ({ mode }) =>
           short_name: loadEnv(mode, process.cwd()).VITE_SITE_NAME,
           description: loadEnv(mode, process.cwd()).VITE_SITE_DES,
           display: "standalone",
-          start_url: "/",
+          start_url: "/home-page/",
           theme_color: "#424242",
           background_color: "#424242",
           icons: [
